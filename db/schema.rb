eabcf26656e68d0e4e1541c1470c6ba1d2a56645
Ju Liu <ju.liu@welaika.com>
2012-10-13 14:58:11 +0200

    user abilities

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121013110605) do
+ActiveRecord::Schema.define(:version => 20121013124517) do
 
   create_table "stores", :force => true do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20121013110605) do
     t.string   "real_name"
     t.integer  "sent_orders"
     t.integer  "completed_orders"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
