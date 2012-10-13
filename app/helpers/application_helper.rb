e545ae7c694cfa29b0a8cb80dfdfeb38a0d57639
Ju Liu <ju.liu@welaika.com>
2012-10-13 11:30:28 +0200

    added some views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def form_error_messages(resource)
+    if resource.errors.present?
+      content_tag :ul, {:class => "form_errors"} do
+        resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join.html_safe
+      end
+    else
+      ""
+    end
+  end
 end
