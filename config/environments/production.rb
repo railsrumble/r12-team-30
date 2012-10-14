90dbf3570c98e55cf7514ed9ab9c414337fc630a
Ju Liu <ju.liu@welaika.com>
2012-10-14 23:50:10 +0200

    production mail is working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,11 @@ Foodstrap::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.mandrillapp.com",
+    :port => 587,
+    :user_name => "ju.liu@welaika.com",
+    :password => "8810cd4c-47ff-4009-affc-9a2f9f52c3d2"
+  }
 end
